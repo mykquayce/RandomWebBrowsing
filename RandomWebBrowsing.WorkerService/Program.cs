afd752f4f2bdcdd7937d2be210426e7ba37f1182
Bob Hall <myk_quayce@hotmail.com>
2019-08-31 16:36:18 +0100

    some tidying

--- RandomWebBrowsing.WorkerService/Program.cs
+++ RandomWebBrowsing.WorkerService/Program.cs
@@ -1,24 +1,22 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace RandomWebBrowsing.WorkerService
 {
-	public class Program
+	public static class Program
 	{
-		public static void Main(string[] args)
+		public static Task Main(string[] args)
 		{
-			CreateHostBuilder(args).Build().Run();
-		}
+			var hostBuilder = Host.CreateDefaultBuilder(args);
 
-		public static IHostBuilder CreateHostBuilder(string[] args) =>
-			Host.CreateDefaultBuilder(args)
+			hostBuilder
 				.ConfigureServices((hostContext, services) =>
 				{
 					services.AddHostedService<Worker>();
 				});
+
+			return hostBuilder.RunConsoleAsync();
+		}
 	}
 }
--- RandomWebBrowsing.WorkerService/Worker.cs
+++ RandomWebBrowsing.WorkerService/Worker.cs
@@ -1,18 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace RandomWebBrowsing.WorkerService
 {
 	public class Worker : BackgroundService
 	{
-		private readonly ILogger<Worker> _logger;
+		private readonly ILogger? _logger;
 
-		public Worker(ILogger<Worker> logger)
+		public Worker(
+			ILogger<Worker>? logger = default)
 		{
 			_logger = logger;
 		}
@@ -21,8 +20,8 @@ namespace RandomWebBrowsing.WorkerService
 		{
 			while (!stoppingToken.IsCancellationRequested)
 			{
-				_logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-				await Task.Delay(1000, stoppingToken);
+				_logger?.LogInformation("Worker running at: {time:u}", DateTimeOffset.Now);
+				await Task.Delay(1_000, stoppingToken);
 			}
 		}
 	}
