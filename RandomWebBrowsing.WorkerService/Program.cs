52d1719150912bebe847b77ffb00c39044508ac6
Bob Hall <myk_quayce@hotmail.com>
2019-08-31 17:54:02 +0100

    copy 'n pasting

--- RandomWebBrowsing.WorkerService/Program.cs
+++ RandomWebBrowsing.WorkerService/Program.cs
@@ -1,6 +1,11 @@
-using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace RandomWebBrowsing.WorkerService
 {
@@ -11,9 +16,76 @@ namespace RandomWebBrowsing.WorkerService
 			var hostBuilder = Host.CreateDefaultBuilder(args);
 
 			hostBuilder
+				.ConfigureAppConfiguration((hostContext, configurationBuilder) =>
+				{
+					if (string.IsNullOrWhiteSpace(hostContext.HostingEnvironment.ApplicationName))
+					{
+						hostContext.HostingEnvironment.ApplicationName = System.Reflection.Assembly.GetAssembly(typeof(Program))!.GetName().Name;
+					}
+
+					var environmentName = Environment.GetEnvironmentVariable("NETCORE_ENVIRONMENT") ?? Environments.Production;
+
+					hostContext.HostingEnvironment.EnvironmentName = environmentName;
+
+					configurationBuilder
+						.SetBasePath(Environment.CurrentDirectory)
+						.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+						.AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true);
+				});
+
+			hostBuilder
 				.ConfigureServices((hostContext, services) =>
 				{
-					services.AddHostedService<Worker>();
+					services
+						.AddHttpClient(
+							nameof(Clients.Concrete.HttpClient),
+							(provider, client) =>
+							{
+								var uriSettings = hostContext.Configuration
+									.GetSection(nameof(Config.Uris))
+									.Get<Config.Uris>();
+
+								client.Timeout = TimeSpan.FromSeconds(10);
+								client.BaseAddress = new Uri(uriSettings.RedditBaseUriString!, UriKind.Absolute);
+								client.DefaultRequestHeaders.Accept.Clear();
+								client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
+							})
+						.ConfigurePrimaryHttpMessageHandler(() =>
+						{
+							return new HttpClientHandler { AllowAutoRedirect = false, };
+						});
+
+					services
+						.AddTransient<Clients.IHttpClient, Clients.Concrete.HttpClient>();
+
+					services
+						.Configure<List<string>>(hostContext.Configuration.GetSection("Blacklist"))
+						.Configure<Helpers.RabbitMQ.Models.Settings>(hostContext.Configuration.GetSection("RabbitMQSettings"))
+						.Configure<Helpers.Jaeger.Models.Settings>(hostContext.Configuration.GetSection("JaegerSettings"))
+						.Configure<Config.Settings>(hostContext.Configuration.GetSection(nameof(Config.Settings)))
+						.Configure<Config.Uris>(hostContext.Configuration.GetSection(nameof(Config.Uris)));
+
+					services
+						.AddJaegerTracing(hostContext.Configuration.GetSection("JaegerSettings"));
+
+					services
+						.AddTransient<Services.IMessageService, Services.Concrete.MessageService>()
+						.AddSingleton<Services.IMessageQueueService, Services.Concrete.MessageQueueService>()
+						.AddTransient<Services.IRedditService, Services.Concrete.RedditService>();
+
+					services
+						.AddTransient<Steps.AcknowledgeMessageStep>()
+						.AddTransient<Steps.ConsumeMessageStep>()
+						.AddTransient<Steps.EvaluateMessageStep>()
+						.AddTransient<Steps.GetSubredditThreadsStep>()
+						.AddTransient<Steps.GetUriRedirectStep>()
+						.AddTransient<Steps.ProcessThreadStep>()
+						.AddTransient<Steps.PublishMessageStep>()
+						.AddTransient<Steps.VisitLinkStep>();
+
+					services
+						.AddHostedService<Worker>()
+						.AddWorkflow();
 				});
 
 			return hostBuilder.RunConsoleAsync();
