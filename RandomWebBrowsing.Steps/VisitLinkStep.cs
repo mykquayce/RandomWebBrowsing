9e84f588af942771dfc8182ced10bb5905d202b3
Bob Hall <myk_quayce@hotmail.com>
2019-08-31 16:21:11 +0100

    adding some logging to visitlinkstep

--- RandomWebBrowsing.Steps/VisitLinkStep.cs
+++ RandomWebBrowsing.Steps/VisitLinkStep.cs
@@ -1,4 +1,5 @@
 ﻿using Dawn;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,11 +12,14 @@ namespace RandomWebBrowsing.Steps
 	public class VisitLinkStep : IStepBody
 	{
 		private readonly Clients.IHttpClient _httpClient;
+		private readonly ILogger? _logger;
 
 		public VisitLinkStep(
-			Clients.IHttpClient httpClient)
+			Clients.IHttpClient httpClient,
+			ILogger<VisitLinkStep>? logger = default)
 		{
 			_httpClient = Guard.Argument(()=> httpClient).NotNull().Value;
+			_logger = logger;
 		}
 
 		public string? UriString { get; set; }
@@ -23,6 +27,8 @@ namespace RandomWebBrowsing.Steps
 		public async Task<ExecutionResult> RunAsync(IStepExecutionContext context)
 		{
 			Guard.Argument(() => UriString).NotNull().NotEmpty().NotWhiteSpace().StartsWith("http");
+
+			_logger?.LogInformation(string.Concat(nameof(VisitLinkStep), ":", UriString));
 
 			var uri = new Uri(UriString, UriKind.Absolute);
 
