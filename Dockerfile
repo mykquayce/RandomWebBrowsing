7a4519f55eaea9ad101f6bf8d12d509fd323a06b
Bob Hall <myk_quayce@hotmail.com>
2019-09-15 07:23:15 +0100

    adding production files: dockerfile, docker-compose.yml, and bulid.sh

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM mcr.microsoft.com/dotnet/core/sdk:3.0 as build-env
+WORKDIR /app
+
+COPY . .
+RUN dotnet restore RandomWebBrowsing.sln -s https://api.nuget.org/v3/index.json -s http://nuget/nuget
+RUN dotnet publish RandomWebBrowsing.WorkerService/RandomWebBrowsing.WorkerService.csproj -c Release -o /app/publish -r linux-x64
+
+FROM mcr.microsoft.com/dotnet/core/runtime:3.0
+ENV NETCORE_ENVIRONMENT=Production
+WORKDIR /app
+COPY --from=build-env /app/publish .
+ENTRYPOINT ["dotnet", "RandomWebBrowsing.WorkerService.dll"]
---
+++ build.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+docker pull mcr.microsoft.com/dotnet/core/runtime:3.0
+docker pull mcr.microsoft.com/dotnet/core/sdk:3.0
+docker pull rabbitmq:management
+
+docker build --tag eassbhhtgu/randomwebbrowsing .
+
+if [ $? -ne 0 ]; then
+   exit 1
+fi
+
+docker secret ls | tail --line +2 | grep randomwebbrowsing_rabbitmq_pass
+
+if [ $? -ne 0 ]; then
+   openssl rand -base64 201 | sed 's/[^0-9A-Za-z]//g' | sed -z 's/\n//g' | docker secret create randomwebbrowsing_rabbitmq_pass -
+fi
+
+docker secret ls | tail --line +2 | grep randomwebbrowsing_rabbitmq_user
+
+if [ $? -ne 0 ]; then
+   openssl rand -base64 60 | sed 's/[^0-9A-Za-z]//g' | sed -z 's/\n//g' | docker secret create randomwebbrowsing_rabbitmq_user -
+fi
+
+docker stack ls | tail --line +2 | grep 'randomwebbrowsing'
+
+if [ $? -ne 0 ]; then
+    docker stack deploy --compose-file ./docker-compose.yml randomwebbrowsing
+else
+    docker service ls | tail --line +2 | grep 'randomwebbrowsing_rabbitmq' | awk '{system("docker service update --image " $5 " " $2)}'
+fi
+
