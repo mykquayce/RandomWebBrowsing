fabd2194feb4e4355752817554e855e03189bc01
Bob <myk_quayce@hotmail.com>
2020-11-15 16:40:23 +0000

    khtkhdctdchkb

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM mcr.microsoft.com/dotnet/sdk:5.0 as build-env
 WORKDIR /app
 
 COPY . .
-RUN dotnet restore RandomWebBrowsing.sln --source https://api.nuget.org/v3/index.json --source http://nuget/nuget
+RUN dotnet restore RandomWebBrowsing.sln --source https://api.nuget.org/v3/index.json --source http://nuget/v3/index.json
 RUN dotnet publish RandomWebBrowsing.WorkerService/RandomWebBrowsing.WorkerService.csproj --configuration Release --output /app/publish --runtime linux-x64
 
 FROM mcr.microsoft.com/dotnet/runtime:5.0
--- build.sh
+++ build.sh
@@ -1,27 +1,42 @@
 #! /bin/bash
 
+# pull Docker images
 cat ./Dockerfile | egrep --ignore-case '^FROM \S+' | awk '{system("docker pull " $2)}'
 cat ./docker-compose.yml | egrep --ignore-case '\bimage: \S+' | awk '{system("docker pull " $2)}'
 
+
+# build our Docker image
 docker build --tag eassbhhtgu/randomwebbrowsing:latest .
 
 if [ $? -ne 0 ]; then
    exit 1
 fi
 
+
+# create Docker secrets (if necessary)
 for s in randomwebbrowsing_rabbitmq_pass randomwebbrowsing_rabbitmq_user
 do
-  docker secret ls | tail --line +2 | grep $s >nul
+  docker secret ls | tail --line +2 | grep $s
 
   if [ $? -ne 0 ]; then
-    openssl rand -base64 201 | sed 's/[^0-9A-Za-z]//g' | sed -z 's/\n//g' | docker secret create $s -
+    openssl rand -base64 201 | \
+        sed 's/[^0-9A-Za-z]//g' | \
+        sed -z 's/\n//g' | \
+        docker secret create $s -
   fi
 done
 
+
+# does Docker Stack exist?
 docker stack ls | tail --line +2 | grep 'randomwebbrowsing'
 
 if [ $? -ne 0 ]; then
+    # create one
     docker stack deploy --compose-file ./docker-compose.yml randomwebbrowsing
 else
-    docker service ls | tail --line +2 | grep 'randomwebbrowsing_rabbitmq' | awk '{system("docker service update --image " $5 " " $2)}'
+    # update it
+    docker service ls | \
+        tail --line +2 | \
+        grep 'randomwebbrowsing_rabbitmq' | \
+        awk '{system("docker service update --image " $5 " " $2)}'
 fi
